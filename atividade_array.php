c6523a52d1d293117de2f4746faa2c1678f3e395
Somaggio098 <gabrielsomaggio7@gmail.com>
2025-08-14 15:38:12 -0300

    primeiro dia

---
+++ atividade_array.php
@@ -0,0 +1,17 @@
+<?php
+    $frutas = [
+        "Maçã",
+        "Pera",
+        "Morango",
+        "Banana",
+        "limâo",
+        "Laranja",
+    ];
+    echo $frutas[0], //Maçã
+
+    $y=4;
+    for($i = $y; $i <= 10; $i++){
+        echo "Numero: $i<br>";
+        }
+
+?>
