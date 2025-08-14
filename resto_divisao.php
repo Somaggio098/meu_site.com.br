781dffbff2fbb3eb4a634525640e185302dd35de
Somaggio098 <gabrielsomaggio7@gmail.com>
2025-08-14 10:39:14 -0300

    versao inicial

---
+++ resto_divisao.php
@@ -0,0 +1,11 @@
+<?php
+$numero1 = 10;
+$numero2 = 3;
+$resto = $numero1 % $numero2;
+$divisao = $numero1 / $numero2;
+
+echo "A divisao de" . $numero1 ."pelo". $numero2."é" .$divisao. "<br>";
+
+echo "resto da divisão é : " .$resto;   
+
+?>
